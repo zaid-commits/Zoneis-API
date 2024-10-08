0a1876f282ed72a2e2e67e3370054c39cf57c151
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-10-08 20:46:17 +0530

    Refactor import statement in auth.js route file

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User= require('../models/User');
+const User= require('../models/User.js');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
