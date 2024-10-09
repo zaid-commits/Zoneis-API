7ae9e05924c4a8cc623ac145786de690b211f42f
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-10-09 17:42:18 +0530

    add route for json format data entries -r1

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -88,4 +88,16 @@ router.post('/login', async (req, res) => {
     }
 });
 
+// Get all users route
+router.get('/users', async (req, res) => {
+    try {
+        const users = await User.find(); // Include password field
+        res.json(users);
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
+
 module.exports = router;
